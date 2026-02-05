dd0a46cc3e91d44fc6398734a13146301de9a0b9
djonte <90456387+djonte@users.noreply.github.com>
2026-02-05 11:58:52 +0100

    feat: add signature verification and unit tests for HMAC (#57) #28
    closes #28

--- src/main/java/ci/CiWebhookController.java
+++ src/main/java/ci/CiWebhookController.java
@@ -3,6 +3,7 @@ package ci;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CiWebhookController {
+
+  @Value("${sharedKey:xxxxxxxx}")
+  private String sharedKey;
 
   @Value("${git.repoName:daDevBoat/ContinuousIntegration}")
   private String repoName;
@@ -29,6 +33,7 @@ public class CiWebhookController {
   @PostMapping("/webhook/github")
   public ResponseEntity<?> githubWebhook(
       @RequestHeader(value = "X-GitHub-Event", required = true) String event,
+      @RequestHeader(value = "X-Hub-Signature-256", required = true) String signature,
       @RequestBody(required = false) byte[] body) {
 
     /* Convert the body[] into a json object */
@@ -45,6 +50,20 @@ public class CiWebhookController {
     if (!ci.Validation.validatePushEvent(event)) {
       return ResponseEntity.badRequest()
           .body("The github event is not push, but is required to be so.");
+    }
+
+    /* Verify signature */
+    boolean signatureValid;
+    try {
+      signatureValid = ci.Validation.validateSignature(sharedKey, body, signature);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body("An internal server error occured when verifying the signature");
+    }
+    if (!signatureValid) {
+      return ResponseEntity.badRequest().body("Signature was invalid");
     }
 
     /* Checking for correct repo */
