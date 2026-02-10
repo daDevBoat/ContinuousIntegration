f17acb26e1ce40f7ead43f200eb5900ed274de04
daDevBoat <113507675+daDevBoat@users.noreply.github.com>
2026-02-10 12:52:42 +0100

    refactor: make CiWebhook controller have a service for running (#100) #96
    close #96

--- src/main/java/ci/CiApplication.java
+++ src/main/java/ci/CiApplication.java
@@ -2,8 +2,10 @@ package ci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CiApplication {
   public static void main(String[] args) {
     SpringApplication.run(CiApplication.class, args);
